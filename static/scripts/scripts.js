495ad3a8f0200d0d62e80b3968035f006b1d7f02
Kieron Scott <kieronscott@kierons-mbp-2.home>
2018-11-06 17:17:31 +0000

    ADD - Dynamic opening times

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,1 +1,8 @@
 document.documentElement.className = "js";
+
+var today = new Date();
+if (today.getDay() == 6 || today.getDay() == 0) {
+  document.getElementById("openingTimes").innerHTML = "09:00 - 17:30";
+} else {
+  document.getElementById("openingTimes").innerHTML = "09:00 - 17:00";
+}
